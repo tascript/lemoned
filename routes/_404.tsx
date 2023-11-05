028bd736622c3ff508c944f759ed25ae216f2e1d
tascript <wataruscript8611@gmail.com>
2023-11-05 23:17:25 +0900

    fix 404

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,15 +6,8 @@ export default function Error404() {
       <Head>
         <title>ページが見つかりません</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
           <h1 class="text-4xl font-bold">ページが見つかりません</h1>
           <p class="my-4">
             アドレスバーに入力されたURLをチェックしてみてね！
